a863a39d02a4714117ca0c5c0eca7db3d1870fb3
adriavieira314 <adriavieira731@gmail.com>
2022-05-09 15:17:46 -0400

    pagina dos andares no ambiente interno

---
+++ lib/models/floors.dart
@@ -0,0 +1,17 @@
+class Floor {
+  final String title;
+
+  Floor(this.title);
+}
+
+final floors = [
+  Floor('Térreo'),
+  Floor('1º Andar'),
+  Floor('2º Andar'),
+  Floor('3º Andar'),
+  Floor('4º Andar'),
+  Floor('5º Andar'),
+  Floor('6º Andar'),
+  Floor('7º Andar'),
+  Floor('8º Andar'),
+];
--- lib/pages/choose_floor_page.dart
+++ lib/pages/choose_floor_page.dart
@@ -1,4 +1,6 @@
+import 'package:atento_totem/models/floors.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class ChooseFloorPage extends StatefulWidget {
   const ChooseFloorPage({Key? key}) : super(key: key);
@@ -8,29 +10,168 @@ class ChooseFloorPage extends StatefulWidget {
 }
 
 class _ChooseFloorPageState extends State<ChooseFloorPage> {
+  int selectedButton = -1;
+
   @override
   Widget build(BuildContext context) {
+    var size = MediaQuery.of(context).size;
+
+    final double itemHeight = (size.height - kToolbarHeight - 24) / 2;
+    final double itemWidth = size.width / 2;
+
     return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
+      body: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
-          const Padding(
-            padding: EdgeInsets.only(bottom: 35.0),
-            child: Text(
-              'Escolha o andar que\n você deseja conhecer:',
-              style: TextStyle(
-                color: Color(0xFF2D6380),
-                fontSize: 25.0,
-                fontWeight: FontWeight.bold,
-                fontFamily: 'Quicksand',
+          Stack(
+            // mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Center(
+                child: Padding(
+                  padding: const EdgeInsets.only(left: 10.0),
+                  child: SizedBox(
+                    width: 80,
+                    height: 80,
+                    child: Material(
+                      child: InkWell(
+                        highlightColor: Colors.transparent,
+                        hoverColor: Colors.transparent,
+                        onTap: (() => Navigator.of(context).pop()),
+                        child: Card(
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(50.0),
+                          ),
+                          elevation: 8,
+                          shadowColor: const Color(0xFFCCCCCC),
+                          child: ShaderMask(
+                            blendMode: BlendMode.srcIn,
+                            shaderCallback: (bounds) => const LinearGradient(
+                              begin: Alignment.bottomRight,
+                              end: Alignment.topLeft,
+                              colors: [
+                                Color(0xFFEDA46E),
+                                Color(0xFFE96F56),
+                              ],
+                            ).createShader(
+                              Rect.fromLTWH(0, 0, bounds.width, bounds.height),
+                            ),
+                            child: const Icon(
+                              Icons.arrow_back,
+                              size: 50.0,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
               ),
-              textAlign: TextAlign.center,
+              ClipRect(
+                child: Align(
+                  alignment: Alignment.bottomRight,
+                  widthFactor: 0.5,
+                  child: SvgPicture.asset(
+                    'assets/images/pink-building.svg',
+                    height: MediaQuery.of(context).size.height * 0.38,
+                  ),
+                ),
+              ),
+            ],
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              const Padding(
+                padding: EdgeInsets.only(bottom: 35.0),
+                child: Text(
+                  'Escolha o andar que\n você deseja conhecer:',
+                  style: TextStyle(
+                    color: Color(0xFF2D6380),
+                    fontSize: 25.0,
+                    fontWeight: FontWeight.bold,
+                    fontFamily: 'Quicksand',
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+              Center(
+                child: SizedBox(
+                  width: 500,
+                  child: GridView.builder(
+                    shrinkWrap: true,
+                    // padding: EdgeInsets.all(16.0),
+                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                      childAspectRatio: (itemWidth / itemHeight),
+                      crossAxisCount: 3,
+                    ),
+                    itemCount: floors.length,
+                    itemBuilder: (BuildContext context, int index) {
+                      return Material(
+                        child: InkWell(
+                          highlightColor: Colors.transparent,
+                          hoverColor: Colors.transparent,
+                          onTap: () {
+                            setState(() {
+                              selectedButton = index;
+                            });
+                          },
+                          child: Card(
+                            margin: const EdgeInsets.all(10.0),
+                            elevation: 5,
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(20.0),
+                            ),
+                            child: Container(
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(20),
+                                gradient: LinearGradient(
+                                  begin: Alignment.topLeft,
+                                  end: Alignment.bottomRight,
+                                  colors: selectedButton == index
+                                      ? [
+                                          const Color(0xFFEDA46E),
+                                          const Color(0xFFE96F56),
+                                        ]
+                                      : [
+                                          Colors.white,
+                                          Colors.white,
+                                        ],
+                                ),
+                              ),
+                              child: Center(
+                                child: Text(
+                                  floors[index].title,
+                                  style: TextStyle(
+                                    color: selectedButton == index
+                                        ? Colors.white
+                                        : const Color(0xFF808080),
+                                    fontSize: 20.0,
+                                    fontWeight: FontWeight.w600,
+                                    fontFamily: 'Quicksand',
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                      );
+                    },
+                  ),
+                ),
+              ),
+            ],
+          ),
+          ClipRect(
+            child: Align(
+              alignment: Alignment.bottomLeft,
+              widthFactor: 0.5,
+              child: SvgPicture.asset(
+                'assets/images/pink-building.svg',
+                height: MediaQuery.of(context).size.height * 0.38,
+              ),
             ),
           ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [],
-          )
         ],
       ),
     );
