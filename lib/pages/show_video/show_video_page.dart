85752d4e7d1fe0a1e131e7a0605ef72cda2e3e32
adriavieira314 <adriavieira731@gmail.com>
2022-07-06 01:09:50 -0400

    botao para sair do video

--- lib/pages/show_video/show_video_page.dart
+++ lib/pages/show_video/show_video_page.dart
@@ -26,10 +26,49 @@ class _ShowVideoPageState extends State<ShowVideoPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Video(
-        player: player,
-        scale: 1.0,
-        showControls: true,
+      body: Stack(
+        children: [
+          Video(
+            player: player,
+            scale: 1.0,
+            showControls: true,
+          ),
+          Positioned(
+            right: 125,
+            top: 50,
+            child: Padding(
+              padding: const EdgeInsets.only(left: 10.0),
+              child: Container(
+                width: 70,
+                height: 70,
+                decoration: const BoxDecoration(
+                  shape: BoxShape.circle,
+                  gradient: LinearGradient(
+                    begin: Alignment.bottomRight,
+                    end: Alignment.topLeft,
+                    colors: [
+                      Color(0xFFEDA46E),
+                      Color(0xFFE96F56),
+                    ],
+                  ),
+                ),
+                child: Material(
+                  color: Colors.transparent,
+                  child: InkWell(
+                    highlightColor: Colors.transparent,
+                    hoverColor: Colors.transparent,
+                    onTap: (() => Navigator.of(context).pop()),
+                    child: const Icon(
+                      Icons.close,
+                      size: 50.0,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
